4d5105430908f03d8d6bc1a8a80ae2b5949c5a8e
Maskouner BHG <florentpainchault@gmail.com>
2026-08-25 22:37:35 +0200

    Création de 3 jokers de test

---
+++ localization/fr.lua
@@ -0,0 +1,42 @@
+return {
+    descriptions =  {
+        Joker = {
+            j_BHM_joker1 = {
+                name = 'Joker 1',
+                text = {
+                    'En gros le joker il donne {C:chips}+#1#{} jetons',
+                    '{C:purple}(Simple comme bonjour !)'
+                }
+            },
+             j_BHM_joker2 = {
+                    name = 'Joker 2',
+                    text = {
+                        {
+                            'Les cartes jouées avec',
+                            'Une couleur {C:clubs}Trefles{} octroient',
+                            '{C:money}#1# ${} en marquant des points'
+                            
+                        },{
+                            'Les gains augmentent aussi de {C:money}#2# $',
+                            'à chaque carte {C:clubs}Trefles{} qui marquent des points',
+                            'pendant la manche',
+                            '{C:inactive}(Réinitialisé à la fin de la manche)'
+                        }
+                    }
+
+             },
+             j_BHM_joker3 = {
+                name = 'Joker 3',
+                text = {
+                    {
+                        '{C:red}+#1#{} défausse a chaque manche'
+                    }, {
+                        'Gagnez {C:money}1 ${} égale au nombre',
+                        'de {C:attention}défausse{} restante à',
+                        'fin de la manche'
+                    }
+                }
+             }
+        }
+    }
+}
--- main.lua
+++ main.lua
@@ -1,4 +1,20 @@
-local Jokers_src = SMODS.NFS.getDirectoryItems(SMODS.current_mod.path .. "src/Jokers")
-for _, file in ipairs(Jokers_src) do
-    assert(SMODS.load_file("src/Jokers/" .. file))()
+--#region Chargement des sprites (Atlas)
+
+SMODS.Atlas {
+    key = 'placeholders',
+    path = 'placeholders.png',
+    px = 71,
+    py = 95
+}
+
+
+--#endregion
+
+
+--#region Chargement des fichier
+local jokers_src = SMODS.NFS.getDirectoryItems(SMODS.current_mod.path .. "src/jokers")
+for _, file in ipairs(jokers_src) do
+    assert(SMODS.load_file("src/jokers/" .. file))()
 end
+
+--#endregion
